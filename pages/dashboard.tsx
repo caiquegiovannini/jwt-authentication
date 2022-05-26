04579ff149d3bafd8171ade846a4562851cdc439
Caique Giovannini <caique_giovannini@hotmail.com>
2022-05-26 00:13:47 -0300

    Get user data in dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import { api } from '../services/api';
 
 const Dashboard = () => {
     const { user } = useContext(AuthContext);
 
+    useEffect(() => {
+        api.get('/me')
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+    }, []);
+
     return (
-        <h1>Dashboard: {user.email}</h1>
+        <h1>Dashboard: {user?.email}</h1>
     );
 };
 
